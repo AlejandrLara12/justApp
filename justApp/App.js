8caf5b14ad09355ae0d9b99e0590a40f108f0a24
AlejandrLara12 <alejandro.elara@live.com>
2017-06-28 09:41:31 -0500

    Creating a loop with data: just display text

--- justApp/App.js
+++ justApp/App.js
@@ -3,11 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let friends = ['oscar','moises','deba','yumi'];
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>This thing is super COOL</Text>
+        <Text>Just trying to get it to work.</Text>
+        { friends.map((friend,index) => <Text key={index.toString()} >{friend}</Text>) }
       </View>
     );
   }
